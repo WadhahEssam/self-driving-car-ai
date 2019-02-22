dabe442706a93c1d314c644283dc973f568a5a33
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-02-22 20:11:45 +0300

    add the Network class along with its constructor

--- ai.py
+++ ai.py
@@ -13,7 +13,7 @@ import torch
 # will take the three signals of the three sensors + the 
 # oreintation and will return the action that the car 
 # should take.
-import torch.nn
+import torch.nn as nn
 # giving a shortcut for the funcational module inside the
 # nn module because it contains the neural network functiuons
 # that we need to use.
@@ -26,3 +26,49 @@ import torch.optim as optim
 # to something else.
 import torch.autograd
 from torch.autograd import Variable
+
+# creating the Network class that is inhereting the nn.Module Class
+# and this class is going to hold every thing our neural network needs
+class Network(nn.Module):
+  # this is how to write the constructor in python ( __init__ )
+  #
+  # self refers to the object of this class that we are going to be
+  # created, it is like this in java except in python you have 
+  # to specify and put it in one the constuctors auguments
+  #
+  # the input size is 5 and it is the signals coming from the 
+  # sensors and the plus and minus orentation, and those are 
+  # coming from the may.py line 
+  def __init__(self, input_size, nb_actions):
+    # this is related to features given by the nn module
+    # and first you need to specify the Network class,
+    # then pass the Network's self object, and then 
+    # chain this function with the .__init__()
+    # so take this for granted
+    super(Network, self).__init__()
+    # attache the arguments with the object attributes
+    self.input_size = input_size
+    self.nb_actions = nb_actions
+    # now we start defining the neural network, by defining
+    # the all the neural network layers and connecting them
+    # together.
+    #
+    # full connection means that all the nerurons in the first
+    # layer is connected to all the neuorons to the second layer.
+    #
+    # so we will start by defingin the neurons for the input layers
+    # and connecting it to the layers of the neurons of the hidden 
+    # layer and this connection is going to be linear which means 
+    # that all neurons in all layers are conntected.
+    #
+    # then we pass the function the numbers of neorons in the input
+    # layer and the number of neurons in the hidden layer.
+    #
+    # the numbes of neurons in the hidden layer is totally expermintal 
+    # so you can play with it until you get a result that satisfy you
+    # the most.
+    #
+    # then we do the same to define the full connection between the 
+    # hidden layer and the output layer 
+    self.full_connection1 = nn.Linear(input_size, 30)
+    self.full_connection2 = nn.Linear(30, nb_actions)
